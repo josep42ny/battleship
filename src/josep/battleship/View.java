4faf72fb5dcb4a9615528e6f25ff5db4c4f8a7d5
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-03-20 10:40:53 +0100

    add numeration to board print

--- src/josep/battleship/View.java
+++ src/josep/battleship/View.java
@@ -3,7 +3,10 @@ package josep.battleship;
 public class View {
 
     public void drawBoard(Board board) {
+
+        System.out.println("   A B C D E F G H I J");
         for (int row = 0; row < board.getRows(); row++) {
+            System.out.print(((row + 1 < 10) ? " " : "") + (row + 1) + " ");
             for (int column = 0; column < board.getColumns(); column++) {
                 System.out.print(board.getTile(row, column));
             }
