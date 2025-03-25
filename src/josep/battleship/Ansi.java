664aa72d4a8c3e9d371b2e0c3c2a45234d89f10c
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-03-25 12:34:53 +0100

    add function to clear cls line

--- src/josep/battleship/Ansi.java
+++ src/josep/battleship/Ansi.java
@@ -1,4 +1,14 @@
 package josep.battleship;
 
-public enum Ansi {
+public class Ansi {
+
+    private static String CURSOR_UP = "\033[F";
+    private static String CLEAR_LINE = "\033[2K";
+
+    public static void clearLine() {
+        System.out.print(CURSOR_UP);
+        System.out.print(CLEAR_LINE);
+        System.out.flush();
+    }
+
 }
---
+++ src/josep/battleship/OutOfBoardException.java
@@ -0,0 +1,7 @@
+package josep.battleship;
+
+public class ColumnParsingException extends RuntimeException {
+    public ColumnParsingException(String message) {
+        super(message);
+    }
+}
