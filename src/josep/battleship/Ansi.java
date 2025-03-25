88849e9e402f5e6a811a4203dde23365a8123b86
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-03-25 13:01:03 +0100

    add clearScreen method

--- src/josep/battleship/Ansi.java
+++ src/josep/battleship/Ansi.java
@@ -3,11 +3,19 @@ package josep.battleship;
 public class Ansi {
 
     private static String CURSOR_UP = "\033[F";
+    private static String CURSOR_TOP = "\033[H";
     private static String CLEAR_LINE = "\033[2K";
+    private static String CLEAR_SCREEN = "\033[2J";
 
     public static void clearLine() {
         System.out.print(CURSOR_UP);
         System.out.print(CLEAR_LINE);
+        System.out.flush();
+    }
+
+    public static void clearScreen() {
+        System.out.print(CURSOR_TOP);
+        System.out.print(CLEAR_SCREEN);
         System.out.flush();
     }
 
--- src/josep/battleship/View.java
+++ src/josep/battleship/View.java
@@ -3,7 +3,7 @@ package josep.battleship;
 public class View {
 
     public void drawBoard(Board board) {
-
+        Ansi.clearScreen();
         System.out.println("   A B C D E F G H I J");
         for (int row = 0; row < board.getRows(); row++) {
             System.out.print(((row + 1 < 10) ? " " : "") + (row + 1) + " ");
