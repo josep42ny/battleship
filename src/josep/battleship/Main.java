65e97b6b6ac9633a98f92a84113303f068013c5d
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-04-01 12:10:11 +0200

    add shutdown hook for estetic purposes

--- src/josep/battleship/Main.java
+++ src/josep/battleship/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.print("\nstopping...\n\n");
+        }));
+
         GameController gameController = new GameController();
         gameController.awake();
 
