af168afe3e0260f4c59523dc0356973965e406e1
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-04-01 13:00:51 +0200

    add color

--- src/josep/battleship/Ansi.java
+++ src/josep/battleship/Ansi.java
@@ -7,8 +7,13 @@ public class Ansi {
     private static String CLEAR_LINE = "\033[2K";
     private static String CLEAR_SCREEN = "\033[2J";
     private static String RESET = "\033[0m";
+    public static String BOLD = "\033[1m";
     public static String BLUE = "\033[44m";
     public static String RED = "\033[41m";
+    public static String BG_BRIGHT_BLACK = "\033[100m";
+    public static String BG_BRIGHT_BLUE = "\033[104m";
+    public static String FG_BRIGHT_RED = "\033[91m";
+    public static String FG_BRIGHT_WHITE = "\033[97m";
 
     public static void clearLine() {
         System.out.print(CURSOR_UP);
--- src/josep/battleship/Tile.java
+++ src/josep/battleship/Tile.java
@@ -3,14 +3,14 @@ package josep.battleship;
 public enum Tile {
 
     FOG(0, "▓▓"),
-    HIT(0, "▀▄"),
-    MISS(0, "XX"),
-    WATER(0, "▒▒"),
-    CARRIER(5, "██"),
-    BATTLESHIP(4, "██"),
-    CRUISER(3, "██"),
-    SUBMARINE(3, "██"),
-    DESTROYER(2, "██");
+    HIT(0, Ansi.paint("╳╳", Ansi.BG_BRIGHT_BLACK + Ansi.FG_BRIGHT_RED + Ansi.BOLD)),
+    MISS(0, Ansi.paint("╺╸", Ansi.BG_BRIGHT_BLUE + Ansi.FG_BRIGHT_WHITE)),
+    WATER(0, Ansi.paint("  ", Ansi.BG_BRIGHT_BLUE)),
+    CARRIER(5, Ansi.paint("  ", Ansi.BG_BRIGHT_BLACK)),
+    BATTLESHIP(4, Ansi.paint("  ", Ansi.BG_BRIGHT_BLACK)),
+    CRUISER(3, Ansi.paint("  ", Ansi.BG_BRIGHT_BLACK)),
+    SUBMARINE(3, Ansi.paint("  ", Ansi.BG_BRIGHT_BLACK)),
+    DESTROYER(2, Ansi.paint("  ", Ansi.BG_BRIGHT_BLACK));
 
     private final String symbol;
     private final int size;
